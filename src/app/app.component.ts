4bc702be115d67ca82f72201f42bdea8d5add349
Arshaan Bhimani <arshaan.bhimani@uwaterloo.ca>
2019-05-21 17:49:25 -0400

    major angular animations working, font changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  } from '@angular/animations';
+import { state, style, transition, animate, trigger, keyframes } from '@angular/animations';
 
 // Colour Palette: https://www.color-hex.com/color-palette/700
 
@@ -7,11 +7,72 @@ import {  } from '@angular/animations';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: []
+  animations: [
+    trigger('headerAnimation', [
+      state('void', style({
+        opacity: 0
+      })),
+      state('notvoid', style({
+        opacity: "*"
+      })),
+      transition('* => *', [
+        animate('5s', keyframes([
+          style({ opacity: 1, offset: 0.2 }),
+          style({ opacity: 1, offset: 0.6 }),
+          style({ opacity: 0, offset: 1 })
+        ]))
+      ])
+    ]),
+    trigger('logoAnimation', [
+      state('void', style({
+        opacity: 0
+      })),
+      state('notvoid', style({
+        opacity: 1
+      })),
+      transition('* => *', [
+        animate('1s 5s ease-in')
+      ])
+    ]),
+    trigger('bodyAnimation', [
+      state('void', style({
+        opacity: 0
+      })),
+      state('notvoid', style({
+        opacity: 1
+      })),
+      transition('* => *', [
+        animate('1s 0.5s ease-in')
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
+  isHeaderAnimation = 'notvoid';
+  isLogoAnimation = 'void';
+  displayHeader = true;
+  displayBody = false;
+  isBodyAnimation = 'void';
 
   ngOnInit() {}
   constructor() {}
+
+  getHeaderAnimation() {
+    return this.isHeaderAnimation;
+  }
+  getLogoAnimation() {
+    return this.isLogoAnimation;
+  }
+  getBodyAnimation() {
+    return this.isBodyAnimation;
+  }
+  onAnimationEventStart(event: AnimationEvent){
+    this.isLogoAnimation = 'notvoid';
+  }
+  onAnimationEventDone(event: AnimationEvent) {
+    this.displayHeader = false;
+    this.displayBody = true;
+    this.isBodyAnimation = 'notvoid';
+  }
 
 }
